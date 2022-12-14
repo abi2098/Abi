7e45346792424ba098dd1ffce37d35524baaf2d0
abi2098 <abiramikumaresan98@gmail.com>
2022-12-14 15:48:38 +0530

    data

---
+++ src/test/java/com/task/Sample.java
@@ -0,0 +1,14 @@
+package com.task;
+
+import org.omg.CosNaming.NamingContextExtPackage.AddressHelper;
+
+public class Sample {
+	private void a() {
+		// TODO Auto-generated method stub
+
+	}
+	private void b() {
+		// TODO Auto-generated method stub
+
+	}
+}
